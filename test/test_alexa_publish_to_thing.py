46dfdd6133a85f37562b25851a92c3a1b1a778f6
thomasmburke <tburke@bu.edu>
2018-10-29 16:58:10 -0400

    created fixtures for testing

--- test/test_alexa_publish_to_thing.py
+++ test/test_alexa_publish_to_thing.py
@@ -1,7 +1,62 @@
 from context import src
 from src.MQTTPublish.alexa_publish_to_thing import SkillHandler
 import json
+import pytest
 
+# Get response config for all pytest fixtures
 with open('../src/MQTTPublish/response_config.json') as f:
     responseConfig = json.load(f)
-print(SkillHandler(event={'request': {'type': 'yumm'},'session': {'application': {'applicationId': 'tommy'}}},responseConfig=responseConfig).intentResponse)
+
+"""
+SET ALL PYTEST FIXTURES
+"""
+
+
+@pytest.fixture
+def launch_skill_handler():
+    '''Returns a SkillHandler instance with a launch request event'''
+    with open('json_requests/launch_request.json') as f:
+        event = json.load(f)
+    return SkillHandler(event=event, responseConfig=responseConfig)
+
+
+@pytest.fixture
+def stop_skill_handler():
+    '''Returns a SkillHandler instance with a stop request event'''
+    with open('json_requests/stop_request.json') as f:
+        event = json.load(f)
+    return SkillHandler(event=event, responseConfig=responseConfig)
+
+
+@pytest.fixture
+def assistance_skill_handler():
+    '''Returns a SkillHandler instance with an assistance request event'''
+    with open('json_requests/assistance_request.json') as f:
+        event = json.load(f)
+    return SkillHandler(event=event, responseConfig=responseConfig)
+
+
+@pytest.fixture
+def fallback_skill_handler():
+    '''Returns a SkillHandler instance with a fallback request event'''
+    with open('json_requests/fallback_request.json') as f:
+        event = json.load(f)
+    return SkillHandler(event=event, responseConfig=responseConfig)
+
+
+@pytest.fixture
+def curtain_close_skill_handler():
+    '''Returns a SkillHandler instance with a curtain close request event'''
+    with open('json_requests/alexa_curtain_intent.json') as f:
+        event = json.load(f)
+    return SkillHandler(event=event, responseConfig=responseConfig)
+
+
+"""
+BEGIN UNIT TESTS
+"""
+
+
+def test_skill_name(launch_skill_handler):
+    assert launch_skill_handler.skillName == 'GPIOControl'
+
