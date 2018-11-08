b25169f705694181ab5215284bbb816fc7ae1cbe
thomasmburke <tburke@bu.edu>
2018-11-07 22:46:45 -0500

    added classifiers and readme to setup

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@
 
 from setuptools import setup, find_packages
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(
     name='src',
     version='0.0.0',
     description='Use alexa to create a smarthome',
-    long_description='more description',
+    long_description=readme(),
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: End Users/Desktop',
+        'Natural Language :: English'
+        ],
     author='Thomas Burke',
     author_email='tburke@bu.edu',
     url='https://github.com/thomasmburke/smarthome',
